43823e7f2fbd4b25ea582307a1c7586d93b09603
Hanna Pshanichnaya <hannapshanich@gmail.com>
2022-08-28 17:14:27 +0200

    Added CSS and buttons

--- src/screens/ChooseProfilePicturePage.js
+++ src/screens/ChooseProfilePicturePage.js
@@ -8,7 +8,9 @@ import { useEffect } from 'react';
 const ChooseProfilePicturePage = () => {
   const [imageUpload, setImageUpload] = useState(null);
   const [imageUrls, setImageUrls] = useState([]);
+  const [morePictures, setMorePictures] = useState(false);
   const imagesListRef = ref(storage, 'images/');
+  const moreImagesListRef = ref(storage, 'moreImages/');
 
   const uploadImage = () => {
     if (imageUpload == null) return;
@@ -25,6 +27,25 @@ const ChooseProfilePicturePage = () => {
     });
   };
 
+  const moreImages = () => {
+    if (imageUpload == null) return;
+    const imageRef = ref(
+      storage,
+      `moreImages/${imageUpload.name + v4()}`
+    ); /* v4 -randomized LETTERS */
+
+    uploadBytes(imageRef, imageUpload).then((snapshot) => {
+      /* uploadFiles - first argument is WHERE you want to upload, second one - WHAT */
+      getDownloadURL(snapshot.ref).then((url) => {
+        setImageUrls((prev) => [...prev, url]);
+      });
+    });
+  };
+
+  const uploadmoreImages = () => {
+    setMorePictures(true);
+  };
+
   useEffect(() => {
     listAll(imagesListRef).then((response) => {
       response.items.forEach((item) => {
@@ -36,24 +57,35 @@ const ChooseProfilePicturePage = () => {
   }, []);
 
   return (
-    <div>
-      <input
-        type="file"
-        onChange={(event) => {
-          setImageUpload(event.target.files[0]);
-        }}
-      />
-      <button onClick={uploadImage}>Upload file</button>
-      {imageUrls.map((url) => {
-        return (
-          <img
-            key={url} /* ?????????????????????  */
-            src={url}
-            alt="firebase"
-            style={{ width: '50px', height: '50px' }}
-          />
-        );
-      })}
+    <div className="container">
+      <h1 className="h1-text">Choose a profile picture:</h1>
+      <div className="profilePictures">
+        {imageUrls.map((url) => {
+          return (
+            <img
+              key={url} /* ?????????????????????  */
+              src={url}
+              alt="firebase"
+              className="profilePicture"
+            />
+          );
+        })}
+        {!morePictures && (
+          <button className="moreProfilePicturesBtn" onClick={uploadmoreImages}>
+            More profile pictures
+          </button>
+        )}
+      </div>
+      <h3 className="h3-text">Or download your own:</h3>
+      <div className="downloadingField">
+        <input
+          type="file"
+          onChange={(event) => {
+            setImageUpload(event.target.files[0]);
+          }}
+        />
+        <button onClick={uploadImage}>Upload file</button>
+      </div>
     </div>
   );
 };
